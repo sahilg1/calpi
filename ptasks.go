211d92363f66c6ca7f07755a7abc4c2ee3cf2f7b
sahilg1 <sahil.gupta432@gmail.com>
2016-06-27 14:46:41 -0700

    updated ptasks.go

--- ptasks.go
+++ ptasks.go
@@ -4,37 +4,63 @@ The program is written in golang.
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net/http"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup
 
-//func init() {
-//runtime.GOMAXPROCS(runtime.NumCPU())
-//}
-
 func main() {
-	wg.Add(1000000)
-	for i := 0; i < 1000000; i++ { //creates 10 million threads. this can be altered to put different load on the CPU
-		go calc() //calls thread to calculate the value of pi
+
+	for {
+		if checkval() {
+			wg.Add(3)
+			for i := 0; i < 3; i++ { //creates 10 million threads. this can be altered to put different load on the CPU
+				go calc() //calls thread to calculate the value of pi
+			}
+			wg.Wait()
+		}
 	}
 
-	wg.Wait()
 }
 
 func calc() { //function to calculate the value of pi
-	var N = 100000000
+	var N = 10000
 	var sum float64
 	var term float64
 	var sign = 1.0
 	for k := 0; k < N; k++ {
-		term = float64((1.0) / (float64(2.0)*float64(k) + float64(1.0)))
-		time.Sleep(2 * time.Millisecond)
-		sum = sum + float64(sign)*term
-		sign = -sign
+		if checkval() {
+			term = float64((1.0) / (float64(2.0)*float64(k) + float64(1.0)))
+			time.Sleep(5 * time.Millisecond)
+			sum = sum + float64(sign)*term
+			sign = -sign
+		} else {
+
+			break
+		}
 	}
 	fmt.Println("Pi=", float64(sum*4.0))
 	wg.Done()
+}
+
+//This function checks the value of the S3 bucket to check to stop or continue running
+func checkval() bool {
+	res, err := http.Get("https://s3.amazonaws.com/sahgupta-cpu-testing/s3file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(res.Body)
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+	if scanner.Text() == "stop" || scanner.Text() == "Stop" {
+		res.Body.Close()
+		return false
+	}
+	res.Body.Close()
+	return true
 }
